8bd8baa01ee8e2d41a2817fdc807449aeef132e2
violentshell <violentshell@gmail.com>
2016-11-29 17:15:32 +1100

    Better PEP8 compliance. Tidied variables and docstrings. Split MB check into function.

--- Rollmac.py
+++ Rollmac.py
@@ -1,8 +1,17 @@
-import requests, spoofmac, psutil, datetime, logging, time, subprocess, json, sys
 import bs4 as soup
+import datetime
+import json
+import logging
+import psutil
+import requests
+import spoofmac
+import subprocess
+import sys
+import time
 
 
-class RollMac():
+# noinspection PyAttributeOutsideInit
+class RollMac:
     def __init__(self):
         self.setup_logging()
         self.get_config()
@@ -30,7 +39,7 @@ class RollMac():
             if self.login() == 200:
                 self.log.info('Reconnected to AP with new mac. Resetting data limit')
 
-                # Reset limits ( Data is doen automatically by flapping interface)
+                # Reset limits ( Data is done automatically by flapping interface)
                 self.over_limit = False
                 self.start_time = datetime.datetime.now()
 
@@ -41,35 +50,27 @@ class RollMac():
             self.over_limit = self.limit_check()
 
     def spoof(self):
-        '''
+        """
         :return: Nothing. Sets mac on interface
-        '''
+        """
         self.mac = spoofmac.random_mac_address()
         spoofmac.set_interface_mac(self.interface, self.mac)
         self.log.debug('Changed mac of {} to: {}'.format(self.interface, self.mac))
 
     def limit_check(self):
-        '''
+        """
         :return: True if within 5% of MB limit; False if outside 5% of MB limit
-        '''
-
-        # Raw interface bytes
-        bytes = psutil.net_io_counters(True)[self.interface]
-
-        # Convert total to MB
-        self.t_mbytes =  (bytes.bytes_recv + bytes.bytes_sent) >>20
-
-        self.log.debug('{} MB remaining'.format(self.limit - self.t_mbytes))
+        """
 
         # Check time limit and MB limit
-        if self.time_check() or self.t_mbytes >= (self.limit - (5 / 100 * self.limit)):
+        if self.time_check() or self.data_check():
             return True
         return False
 
     def time_check(self):
-        '''
+        """
         :return: True if within 5 min of limit; False if outside 5 min of limit
-        '''
+        """
 
         try:
             self.start_time
@@ -84,43 +85,55 @@ class RollMac():
         self.log.info('{} minutes remaining'.format(round(self.time_limit - online_time - 5)))
 
         # Return True if over limit threshold
-        if self.time_limit - online_time <= 5 :
-            self.log.warn('Over Time Limit')
+        if self.time_limit - online_time <= 5:
+            self.log.warn('Over time limit.')
+            return True
+        return False
+
+    def data_check(self):
+        # Raw interface bytes TODO Linux compatible?
+        netbytes = psutil.net_io_counters(True)[self.interface]
+
+        # Convert total to MB
+        t_mbytes = (netbytes.bytes_recv + netbytes.bytes_sent) >> 20
+
+        # For info
+        self.log.debug('{} MB remaining'.format(self.limit - t_mbytes))
+
+        # Return True if over limit threshold
+        if t_mbytes >= (self.limit - (5 / 100 * self.limit)):
+            self.log.warn('Over data limit.')
             return True
         return False
 
     def connect(self):
-        '''
-        :param profile: A string representing the ssid profile name in netsh
+        """
         :return: True if connected, False for fail
-        '''
+        """
 
         netsh = subprocess.check_output('netsh wlan connect name="{}"'.format(self.ssid))
         if b'Connection request was completed successfully.' in netsh:
-             return self.log.debug('Re-assosiated with {}'.format(self.ssid))
+            return self.log.debug('Re-assosiated with {}'.format(self.ssid))
 
-
-    def retry_get(self,url):
-        '''
-        :param url: string of a url
+    def retry_get(self, url):
+        """
+        :param url: string of a url to fetch
         :return: data if it works, raises error if not
-        '''
+        """
         for i in range(5):
             try:
                 data = requests.get(url)
                 return data
             except:
-                self.log.warn('Get failed for {} try number {}'.format(url,i))
+                self.log.warn('Get failed for {} try number {}'.format(url, i))
                 time.sleep(3)
 
         raise ConnectionError
 
     def get_redirect(self):
-        '''
-
-        :param self.info['domain']: The address of the wpad server: 'wpad.domain.com'
+        """
         :return: url of the page redirected to from wpad
-        '''
+        """
         wpad_data = self.retry_get('http://wpad.{}'.format(self.info['domain']))
 
         if wpad_data.status_code != 200:
@@ -134,9 +147,9 @@ class RollMac():
         self.log.info('Found redirect url: {}'.format(self.info['url']))
 
     def login(self):
-        '''
+        """
         :return: Status code for our post to the login page
-        '''
+        """
         logon_page = self.retry_get(self.info['url'])
 
         so2 = soup.BeautifulSoup(logon_page.content, 'lxml')
@@ -157,7 +170,6 @@ class RollMac():
         try:
             with open('conf.json', 'r') as f:
                 self.config = json.load(f)
-
             self.ssid = self.config['ssid']
             self.limit = self.config['MB_limit']
             self.time_limit = self.config['TIME_limit']
@@ -177,25 +189,23 @@ class RollMac():
         self.format = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
         # Main logger
-        rootLogger = logging.getLogger()
-        rootLogger.setLevel(logging.DEBUG)
+        rootlogger = logging.getLogger()
+        rootlogger.setLevel(logging.DEBUG)
 
         # Print to console
-        consoleHandler = logging.StreamHandler()
-        consoleHandler.setFormatter(self.format)
-        rootLogger.addHandler(consoleHandler)
+        consolehandler = logging.StreamHandler()
+        consolehandler.setFormatter(self.format)
+        rootlogger.addHandler(consolehandler)
 
         # Good to go
         self.log.debug('Logging setup complete')
 
 
-if __name__ =='__main__':
+if __name__ == '__main__':
+
     # Init class
     main = RollMac()
 
     # Loop forever
     while True:
         main.run()
-
-
-
